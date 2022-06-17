fb97182d43dcd4501f198ea3a74f9a35150172c7
joyannavb <joyannavb@gmail.com>
2022-06-17 15:58:18 -0500

    Completed all tasks

--- CollectionsMasterConsoleUI/Program.cs
+++ CollectionsMasterConsoleUI/Program.cs
@@ -7,131 +7,133 @@ namespace CollectionsMasterConsoleUI
     {
         static void Main(string[] args)
         {
-            //TODO: Follow the steps provided in the comments under each region.
-            //Make the console formatted to display each section well
-            //Utlilize the method stubs at the bottom for the methods you must create ⬇⬇⬇
-
+          
             #region Arrays
-            //TODO: Create an integer Array of size 50
             
+            int[] Numbers = new int[50];
+            Populater(Numbers);
 
-            //TODO: Create a method to populate the number array with 50 random numbers that are between 0 and 50
-            
-
-            //TODO: Print the first number of the array
-
-            //TODO: Print the last number of the array            
+            Console.WriteLine(Numbers[0]);
+            Console.WriteLine(Numbers[49]);
 
             Console.WriteLine("All Numbers Original");
-            //UNCOMMENT this method to print out your numbers from arrays or lists
-            //NumberPrinter();
+                NumberPrinter(Numbers);
             Console.WriteLine("-------------------");
-
-            //TODO: Reverse the contents of the array and then print the array out to the console.
-            //Try for 2 different ways
-            /*  1) First way, using a custom method => Hint: Array._____(); 
-                2) Second way, Create a custom method (scroll to bottom of page to find ⬇⬇⬇)
-            */
 
             Console.WriteLine("All Numbers Reversed:");
-
             Console.WriteLine("---------REVERSE CUSTOM------------");
-
+                ReverseArray(Numbers);
             Console.WriteLine("-------------------");
-
-            //TODO: Create a method that will set numbers that are a multiple of 3 to zero then print to the console all numbers
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
             Console.WriteLine("Multiple of three = 0: ");
-            
-
+                ThreeKiller(Numbers);
             Console.WriteLine("-------------------");
 
-            //TODO: Sort the array in order now
-            /*      Hint: Array.____()      */
             Console.WriteLine("Sorted numbers:");
-            
+                Array.Sort(Numbers);
+                NumberPrinter(Numbers);
 
             Console.WriteLine("\n************End Arrays*************** \n");
             #endregion
 
             #region Lists
             Console.WriteLine("************Start Lists**************");
-
-            /*   Set Up   */
-            //TODO: Create an integer List
-            
-
-            //TODO: Print the capacity of the list to the console
-            
-
-            //TODO: Populate the List with 50 random numbers between 0 and 50 you will need a method for this            
-            
-
-            //TODO: Print the new capacity
-            
-
+                var numberList = new List<int>();
+                Console.WriteLine($"List capacity: {numberList.Capacity}");   
+                Populater(numberList);
+                Console.WriteLine($"New list capacity: {numberList.Capacity}");
             Console.WriteLine("---------------------");
 
-            //TODO: Create a method that prints if a user number is present in the list
-            //Remember: What if the user types "abc" accident your app should handle that!
             Console.WriteLine("What number will you search for in the number list?");
-            
-            Console.WriteLine("-------------------");
+                int searchNumber;
+                bool isNumber;
 
+                do
+                {
+                isNumber = int.TryParse(Console.ReadLine(), out searchNumber);
+                } while (isNumber == false);
+
+                NumberChecker(numberList, searchNumber);
+            Console.WriteLine("-------------------");
+           
             Console.WriteLine("All Numbers:");
-            //UNCOMMENT this method to print out your numbers from arrays or lists
-            //NumberPrinter();
+                NumberPrinter(numberList);
             Console.WriteLine("-------------------");
 
-
-            //TODO: Create a method that will remove all odd numbers from the list then print results
             Console.WriteLine("Evens Only!!");
-            
+                OddKiller(numberList);
             Console.WriteLine("------------------");
 
-            //TODO: Sort the list then print results
             Console.WriteLine("Sorted Evens!!");
-            
+                numberList.Sort();
+                NumberPrinter(numberList);
             Console.WriteLine("------------------");
 
-            //TODO: Convert the list to an array and store that into a variable
-            
+                int[] convertNumbers = numberList.ToArray();
+                var randomNumbers = convertNumbers;
+                numberList.Clear();
 
-            //TODO: Clear the list
-            
 
             #endregion
         }
 
         private static void ThreeKiller(int[] numbers)
         {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i]%3 == 0)
+                {
+                    numbers[i] = 0;
+                }
+                NumberPrinter(numbers);
+            }
             
         }
 
         private static void OddKiller(List<int> numberList)
         {
-            
+            for (int i = numberList.Count -1; i>-1; i--)
+            {
+                if (numberList[i]%2 != 0)
+                {
+                    numberList.Remove(numberList[i]);
+                }
+            }
+            NumberPrinter(numberList);
         }
 
         private static void NumberChecker(List<int> numberList, int searchNumber)
         {
-            
+            if (numberList.Contains(searchNumber))
+                { Console.WriteLine($"Nice job! you guessed one of {numberList.Count} numbers."); }
+            else
+                { Console.WriteLine("Better luck next time. We don't have that number."); }
         }
 
         private static void Populater(List<int> numberList)
         {
-            Random rng = new Random();
-
+            for (int i = 0; i < 50; i++)
+            {
+                Random rng = new Random();
+                int number= rng.Next(0, 50);
+                numberList.Add(number);
+            }
         }
 
         private static void Populater(int[] numbers)
         {
-            Random rng = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+              Random rng = new Random();
+              numbers[i] = rng.Next(0, 50);
+            }
 
         }        
 
         private static void ReverseArray(int[] array)
         {
-            
+            Array.Reverse(array);
+            NumberPrinter(array);
         }
 
         /// <summary>
